9a302cc2619e090a39c3986c0c056fd1da45073c
Sumeet Wagh <sumeetwagh70@gmail.com>
2022-06-23 23:08:27 +0530

    Comments Added

--- controllers/email.js
+++ controllers/email.js
@@ -8,7 +8,7 @@ export const getAmount = async (req, res) => {
     const {city, start, end} = req.body;
 
     if(!city){
-        res.json({email: true});                // req
+        res.json({email: true});                // required
         return;
     }else if(city=='london' || city=='paris'){
         res.json({email: false});
